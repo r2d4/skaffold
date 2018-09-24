3001f5f299f17a20c9d6b94da4fe2c7bb6db657e
Matt Rickard <mrick@google.com>
2018-09-24 10:18:02 -0700

    changes

--- pkg/skaffold/kubernetes/sync.go
+++ pkg/skaffold/kubernetes/sync.go
@@ -12,36 +12,41 @@ import (
 )
 
 func CopyFilesForImage(image string, f []string) error {
+	return perform(image, f, copyFileFn)
+}
+
+func DeleteFilesForImage(image string, f []string) error {
+	return perform(image, f, deleteFileFn)
+}
+
+func deleteFileFn(pod v1.Pod, container v1.Container, file string) *exec.Cmd {
+	return exec.Command("kubectl", "exec", fmt.Sprintf("%s/%s", pod.Namespace, pod.Name), "-c", container.Name, "rm", "-rf", file)
+}
+
+func copyFileFn(pod v1.Pod, container v1.Container, file string) *exec.Cmd {
+	return exec.Command("kubectl", "cp", file, fmt.Sprintf("%s/%s:%s", pod.Namespace, pod.Name, file), "-c", container.Name)
+}
+
+func perform(image string, files []string, cmdFn func(v1.Pod, v1.Container, string) *exec.Cmd) error {
 	client, err := Client()
 	if err != nil {
 		return errors.Wrap(err, "getting k8s client")
 	}
 	pods, err := client.CoreV1().Pods("").List(meta_v1.ListOptions{})
 	if err != nil {
-		return errors.Wrap(err, "getting all pods")
+		return errors.Wrap(err, "getting pods")
 	}
 	for _, p := range pods.Items {
 		for _, c := range p.Spec.Containers {
 			if strings.HasPrefix(c.Image, image) {
-				if err := copy(p, c, f); err != nil {
-					return errors.Wrap(err, "copying files into pod")
+				for _, f := range files {
+					cmd := cmdFn(p, c, f)
+					if err := util.RunCmd(cmd); err != nil {
+						return errors.Wrap(err, "running kubectl cp")
+					}
 				}
 			}
 		}
 	}
-	return nil
-}
-
-func copy(pod v1.Pod, container v1.Container, files []string) error {
-	for _, f := range files {
-		cmd := exec.Command("kubectl", "cp", f, fmt.Sprintf("%s/%s:%s", pod.Namespace, pod.Name, f), "-c", container.Name)
-		if err := util.RunCmd(cmd); err != nil {
-			return errors.Wrap(err, "running kubectl cp")
-		}
-	}
-	return nil
-}
-
-func DeleteFilesForImage(image string, f []string) error {
 	return nil
 }
